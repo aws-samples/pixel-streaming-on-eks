78f020b72423f0d780eb1c29b0c905afe029dbdf
Peter Heneback <heneback@amazon.co.uk>
2024-06-21 19:02:35 +0100

    Removed hard coded regions and creds

--- packer/scripts/setup_gpu.sh
+++ packer/scripts/setup_gpu.sh
@@ -4,7 +4,6 @@ set -eu
 # Install nvidia-container-toolkit: https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html#installing-with-yum-or-dnf
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | \
 sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
-sudo yum-config-manager --enable nvidia-container-toolkit-experimental
 sudo yum install -y nvidia-container-toolkit
 
 
@@ -17,21 +16,25 @@ echo "* * Installing the NVIDIA drivers * *"
 sudo CC=/usr/bin/gcc10-cc ./NVIDIA-Linux-x86_64*.run --silent
 
 echo "* * Setting up NVIDIA drivers for containerd * *"
-sudo touch /etc/modprobe.d/nvidia.conf
-echo "options nvidia NVreg_EnableGpuFirmware=0" | sudo tee --append /etc/modprobe.d/nvidia.conf
 
-# Configuration for containerd to use GPU
-sudo sed -i -e "s/default_runtime_name = \"runc\"/default_runtime_name = \"nvidia\"/g" /etc/eks/containerd/containerd-config.toml
-sudo tee -a /etc/eks/containerd/containerd-config.toml << EOS
+sudo nvidia-ctk runtime configure --runtime=containerd
+sudo nvidia-ctk runtime configure --runtime=containerd
 
-[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.nvidia]
-  privileged_without_host_devices = false
-  runtime_engine = ""
-  runtime_root = ""
-  runtime_type = "io.containerd.runc.v1"
-[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.nvidia.options]
-   BinaryName = "/usr/bin/nvidia-container-runtime"
-EOS
+# sudo touch /etc/modprobe.d/nvidia.conf
+# echo "options nvidia NVreg_EnableGpuFirmware=0" | sudo tee --append /etc/modprobe.d/nvidia.conf
+
+# # Configuration for containerd to use GPU
+# sudo sed -i -e "s/default_runtime_name = \"runc\"/default_runtime_name = \"nvidia\"/g" /etc/eks/containerd/containerd-config.toml
+# sudo tee -a /etc/eks/containerd/containerd-config.toml << EOS
+
+# [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.nvidia]
+#   privileged_without_host_devices = false
+#   runtime_engine = ""
+#   runtime_root = ""
+#   runtime_type = "io.containerd.runc.v1"
+# [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.nvidia.options]
+#    BinaryName = "/usr/bin/nvidia-container-runtime"
+# EOS
 
 echo "* * Restarting containerd * *"
 sudo systemctl restart containerd
--- scripts/deploy_demo.sh
+++ scripts/deploy_demo.sh
@@ -9,7 +9,7 @@ fi
 REPLICAS=$1
 REPLICAS_PER_NODE=2
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
-REGION=eu-west-2
+REGION=$(aws configure get region)
 PORT_START=50000
 NODE_PORT_START=30000
 APP_TEMPLATE_FILE=./manifests/pixel-streaming.template.yaml
